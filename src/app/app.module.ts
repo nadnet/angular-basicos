9100a6cbb4434df104e4afcdc66e71d49edbea31
nadia <nadia.carolina1@gmail.com>
2022-08-09 16:31:55 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroeModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
